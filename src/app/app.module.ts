f632a9a9a26edc8ae8daffa50a275fed7ce8340c
Sweng29 <khosokashif@gmail.com>
2019-07-21 01:10:05 +0500

    added navigations sidebars and dealer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {LeftSidebarComponent} from './shared/left-sidebar/left-sidebar.component';
+import {RightSidebarComponent} from './shared/right-sidebar/right-sidebar.component';
+import {UserComponent} from './users/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
